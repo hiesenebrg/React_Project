9310bdb8ccb43428f224978624b4c3997b88d08f
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-09 20:21:29 +0530

    Custom-Fetch-2

--- src/api/index.js
+++ src/api/index.js
@@ -1,49 +1,50 @@
-import { LOCALSTORAGE_TOKEN_KEY } from "../utils";
+import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from "../utils";
 
-const customfetch = async (url , {body ,...customConfig})=>{
-    const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
-   
-    const headers = {
-        'content-type': 'application/json',
-    Accept : 'application/json'
-    }
+const customfetch = async (url, { body, ...customConfig }) => {
+  const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
 
-    if(token){
-       headers.Authorization = `Bearer ${token}`; 
-    }
+  const headers = {
+    "content-type": "application/json",
+    Accept: "application/json",
+  };
 
-    const config = {
-        ...customConfig,
-        headers:{
-            ...headers ,
-            ...customConfig.headers,
-        },
-    };
-    if(body){
-        config.body = JSON.strigyfy(body);
-    }
-     
-   try {
-    const response = await fetch(url , config);
-    const data = response.data;
-    if(response.success){
-        return{
-            data:data.data,
-            success:true
-        };
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+  if (body) {
+    config.body = JSON.strigyfy(body);
+  }
+
+  try {
+    const response = await fetch(url, config);
+    const data = await response.json();
+    if (data.success) {
+      return {
+        data: data.data,
+        success: true,
+      };
     }
 
     throw new Error(data.message);
-   } catch (error) {
-    console.log('error');
+  } catch (error) {
+    console.log("error");
     return {
-        message:error.message,
-        success:false,
-    }
-   } 
+      message: error.message,
+      success: false,
+    };
+  }
 };
 
-const getPosts = (page , limit)=>{
-    
-    return customfetch();
-}
+export const getPosts = (page = 1, limit = 5) => {
+  return customfetch(API_URLS.posts(page, limit), {
+    method: "GET",
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { App } from "./components";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
