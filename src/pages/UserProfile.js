2c28f6bd91e1cbe3b5291fe9d2f6c3293790ae81
hiesenebrg <adarshsingh8008@gmail.com>
2022-11-25 14:33:00 +0530

    improving user profile logic

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,18 +1,55 @@
-import { useState } from 'react';
-import { useToasts } from 'react-toast-notifications';
-import { useLocation } from 'react-router-dom';
-import styles from '../styles/settings.module.css';
-import { useAuth } from '../hooks';
-
+import { useState } from "react";
+import { useEffect } from "react";
+import { useToasts } from "react-toast-notifications";
+import { useParams, useNavigate } from "react-router-dom";
+import styles from "../styles/settings.module.css";
+import { useAuth } from "../hooks";
+import { Loader } from "../components";
+import { fetchUserProfile } from "../api";
 const UserProfile = () => {
-  const location = useLocation();
-  console.log('location', location);
-  // it is grabing the user state from the home page
-  // also it is sused for basically supoose someone copied the profile page url
-  // then he or she can access that page but it prevents from it because if someone copied the url
-// and paste in another tab then it gives error because our user stae is still be empty 
-  const { user = {} } = location.state;
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { userId } = useParams();
+  const { addToast } = useToasts();
+  const history = useNavigate()();
+  const auth = useAuth();
+// in order to make any user see the profile page using the link is we have
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetchUserProfile(userId);
 
+      if (response.success) {
+        setUser(response.data.user);
+      } else {
+        addToast(response.message, {
+          appearance: "error",
+        });
+        return history.push("/");
+      }
+
+      setLoading(false);
+    };
+
+    getUser();
+  }, [userId, history, addToast]);
+  // it means that if userId or history or addToast is changes then our useEffect function will re-run
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  const checkIfUserIsAFriend = () => {
+    const friends = auth.user.friends;
+
+    const friendIds = friends.map((friend) => friend.to_user._id);
+    const index = friendIds.indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  };
 
   return (
     <div className={styles.settings}>
@@ -35,9 +72,12 @@ const UserProfile = () => {
       </div>
 
       <div className={styles.btnGrp}>
-        <button className={`button ${styles.saveBtn}`}>Add friend</button>
-
-        <button className={`button ${styles.saveBtn}`}>Remove friend</button>
+        {/* id user is already friends then show remove friend */}
+      {checkIfUserIsAFriend() ? (
+          <button className={`button ${styles.saveBtn}`}>Remove friend</button>
+        ) : (
+          <button className={`button ${styles.saveBtn}`}>Add friend</button>
+        )}
       </div>
     </div>
   );
